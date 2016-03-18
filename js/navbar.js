697a142c497aa50cb09dbab6a4d5ee9f62131fb4
bonbombs <kelly.zhang2306@gmail.com>
2016-03-18 13:48:00 -0400

    navbar generates separately

---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+document.write("<nav class='navbar navbar-default navbar-fixed-bottom'>\
+    <div class='container'>\
+        <div class='navbar-header'>\
+            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-expanded='navbar'>\
+                <span class='sr-only'>Toggle navigation</span>\
+                <span class='icon-bar'></span>\
+                <span class='icon-bar'></span>\
+                <span class='icon-bar'></span>\
+            </button>\
+            <a class='navbar-brand' href='/index.html'>Kelly Zhang's Portfolio</a>\
+        </div>\
+        <div id='navbar' class='collapse navbar-collapse navbar-right' id='navbar'>\
+            <ul class='nav navbar-nav'>\
+                <li role='presentation' class='dropdown'>\
+                    <a class='dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>\
+                    Art <span class='caret'></span>\
+                    </a>\
+                    <ul class='dropdown-menu'>\
+                        <li><a href='/gallery/index.html'>Main</a></li>\
+                        <li><a href='/gallery/conceptart.html'>Concept Art</a></li>\
+                        <li><a href='#'>3D</a></li>\
+                    </ul>\
+                </li>\
+                <!--<li><a href='/projects.html'>Projects</a></li>-->\
+                <li><a href='/gamejams.html'>Game Jams</a></li>\
+            </ul>\
+        </div><!--/.nav-collapse -->\
+    </div>\
+</nav>");
