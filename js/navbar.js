180fc2d70a20df284ec1e21ca603ab48273c9755
bonbombs <kelly.zhang2306@gmail.com>
2016-03-24 10:18:37 -0400

    added more stuff

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,30 @@
-document.write("<nav class='navbar navbar-default navbar-fixed-bottom'>\
-    <div class='container'>\
-        <div class='navbar-header'>\
-            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-expanded='navbar'>\
-                <span class='sr-only'>Toggle navigation</span>\
-                <span class='icon-bar'></span>\
-                <span class='icon-bar'></span>\
-                <span class='icon-bar'></span>\
-            </button>\
-            <a class='navbar-brand' href='/index.html'>Kelly Zhang's Portfolio</a>\
-        </div>\
-        <div id='navbar' class='collapse navbar-collapse navbar-right' id='navbar'>\
-            <ul class='nav navbar-nav'>\
-                <li role='presentation' class='dropdown'>\
-                    <a class='dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>\
-                    Art <span class='caret'></span>\
-                    </a>\
-                    <ul class='dropdown-menu'>\
-                        <li><a href='/gallery/index.html'>Main</a></li>\
+document.write("<div class='naviagtion'>\
+            <div class='header'>\
+                <span id='name'>Kelly Zhang</span>\
+            </div>\
+            <ul class='topnav'>\
+                <li class='mobile-header'>Kelly Zhang</li>\
+                <li><a href='/index.html'>Home</a></li>\
+                <li>\
+                    <a href='#'>Art</a>\
+                    <ul class='drop'>\
+                        <li><a href='/gallery/index.html'>Gallery</a></li>\
                         <li><a href='/gallery/conceptart.html'>Concept Art</a></li>\
                         <li><a href='#'>3D</a></li>\
                     </ul>\
                 </li>\
-                <!--<li><a href='/projects.html'>Projects</a></li>-->\
-                <li><a href='/gamejams.html'>Game Jams</a></li>\
+                <li><a href='/projects.html'>Projects</a></li>\
+                <li><a href='#'>Contact</a></li>\
+                <li class='icon'>\
+                    <button type='button' class='btn btn-default' onclick='myFunction()'>\
+                        <span class='sr-only'>Toggle navigation</span>\
+                        <span class='glyphicon glyphicon-menu-hamburger'></span>\
+                    </button>\
+                </li>\
             </ul>\
-        </div><!--/.nav-collapse -->\
-    </div>\
-</nav>");
+        </div>\
+        <script>\
+            function myFunction() {\
+                document.getElementsByClassName('topnav')[0].classList.toggle('responsive');\
+            }\
+        </script>");
